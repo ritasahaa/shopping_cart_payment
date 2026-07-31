93326823882a89f452f3ffdf40fd31f08c54aead
Rita kumari sah <ritasah1602@gmail.com>
2026-07-31 22:41:49 +0530

    minor bug fix

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -6,9 +6,7 @@ function Cart() {
       const BASE_URL =
         import.meta.env.MODE === "development"
           ? "http://localhost:5000"
-          : "https://shopping-cart-payment-server.onrender.com"
-
-";
+          : "https://shopping-cart-payment-server.onrender.com";
 
       await axios.post(`${BASE_URL}/api/payment/create-order`, {
         amount: 499,
